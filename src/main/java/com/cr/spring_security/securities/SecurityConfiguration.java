d35c0d8f7d57a38f43ea6c31e9ce1f2aa9a5cd26
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-05-25 22:07:56 -0300

    feat: permitindo acesso geral para Get

--- src/main/java/com/cr/spring_security/securities/SecurityConfiguration.java
+++ src/main/java/com/cr/spring_security/securities/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.cr.spring_security.securities;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Controller;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    //Isso configura que qualquer usuário pode utilizar o get sem estar verificado
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+                .authorizeHttpRequests(auth -> auth.requestMatchers
+                        (HttpMethod.GET, "/pessoas").permitAll()).build();
     }
 
 }
